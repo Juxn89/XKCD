a9dcf3bfd364bbe7776cd2b9e0e99514b4dcfbff
Juan Gómez <gb.jc@outlook.com>
2019-11-08 22:24:28 -0600

    Task-2: Se eliminan Actions innecesarias y a su vez se realizar la inyeccción de dependencia para obtener el comic del día atravéz de la API.

--- XKCD-Comic/Controllers/HomeController.cs
+++ XKCD-Comic/Controllers/HomeController.cs
@@ -5,33 +5,23 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using XKCD_Comic.Models;
+// MIS REFERENCIAS
+using XKCD_Comic.BLL;
 
 namespace XKCD_Comic.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        public IComic _comic;
+        public HomeController(IComic comic)
         {
-            _logger = logger;
+            _comic = comic;
         }
 
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            var info = _comic.TodaysComic();
+            return View(info);
         }
     }
 }
