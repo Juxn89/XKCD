f3070e2e552116fc46d26cb7397a5536e4d22067
Juan Gómez <gb.jc@outlook.com>
2019-11-08 22:55:48 -0600

    Task-2: Se agrega el controlador Comic y con la ruta Comic/XXX

---
+++ XKCD-Comic/Controllers/ComicController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace XKCD_Comic.Controllers
+{
+    public class ComicController : Controller
+    {
+        public ComicController() {
+
+        }
+
+        [HttpGet("Comic/{id}")]
+        public IActionResult Index(int id)
+        {
+            return View();
+        }
+    }
+}
