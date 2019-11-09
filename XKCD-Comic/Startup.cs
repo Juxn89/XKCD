0f1c1ad6aeeb73f4dfed7f0dd583da02291d3973
Juan Gómez <gb.jc@outlook.com>
2019-11-08 21:51:54 -0600

    Task-2: Se agrega a la clase Startup.cs el servicio de la interfaz, para aplicar la inyección de dependencias.

--- XKCD-Comic/Startup.cs
+++ XKCD-Comic/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+// MIS REFERENCIAS
+using XKCD_Comic.BLL;
 
 namespace XKCD_Comic
 {
@@ -24,6 +26,7 @@ namespace XKCD_Comic
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddTransient<IComic, Comic>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
