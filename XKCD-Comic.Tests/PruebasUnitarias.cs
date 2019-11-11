0725024da407693f36f145317997da5661c5b82b
Juan Gómez <gb.jc@outlook.com>
2019-11-11 09:12:30 -0600

    Task-5: Se agregan las pruebas unitatias para para validar un cómic anterior y validar cuándo un número de cómic no existe.

--- XKCD-Comic.Tests/PruebasUnitarias.cs
+++ XKCD-Comic.Tests/PruebasUnitarias.cs
@@ -23,5 +23,32 @@ namespace XKCD_Comic.Tests
             Assert.NotEmpty(data.Result.title);
             Assert.NotEmpty(data.Result.img);
         }
+
+        [Fact]
+        public void Get_Previous_Comic()
+        {
+            Random random = new Random();
+            int numComic = random.Next(1, 2226);
+
+            var data = _comic.PreviousComic(numComic);
+
+            Assert.NotNull(data.Result);
+            Assert.NotEmpty(data.Result.alt);
+            Assert.NotEmpty(data.Result.title);
+            Assert.NotEmpty(data.Result.img);
+        }
+
+        [Fact]
+        public void Validar_Comic_Que_No_Existe()
+        {
+            var data = _comic.PreviousComic(404);
+
+            Assert.NotNull(data.Result);
+            Assert.NotEmpty(data.Result.alt);
+            Assert.Null(data.Result.title);            
+            Assert.NotEmpty(data.Result.img);
+            Assert.True(data.Result.img == "../assets/img/404.jpg");
+            Assert.True(data.Result.httpStatus == 404);
+        }
     }
 }
