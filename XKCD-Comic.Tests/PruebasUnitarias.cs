be4fa2f7921cb3ea6731b0eaa95914852ad28933
Juan Gómez <gb.jc@outlook.com>
2019-11-11 04:16:00 -0600

    Task-5: Se crea la prueba unitaria para obtener el comic del día.

--- XKCD-Comic.Tests/PruebasUnitarias.cs
+++ XKCD-Comic.Tests/PruebasUnitarias.cs
@@ -1,14 +1,27 @@
 using System;
 using Xunit;
+// MIS REFERENCIAS
+using XKCD_Comic.BLL;
 
 namespace XKCD_Comic.Tests
 {
     public class PruebasUnitarias
     {
+        public Comic _comic;
+
+        public PruebasUnitarias() {
+            _comic = new Comic();
+        }
+
         [Fact]
         public void Get_Todays_Comic()
         {
+            var data = _comic.TodaysComic();
 
+            Assert.NotNull(data.Result);
+            Assert.NotEmpty(data.Result.alt);
+            Assert.NotEmpty(data.Result.title);
+            Assert.NotEmpty(data.Result.img);
         }
     }
 }
