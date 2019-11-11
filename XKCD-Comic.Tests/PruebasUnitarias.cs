e66a64260668808f75dece7e330633beed32e516
Juan Gómez <gb.jc@outlook.com>
2019-11-11 03:26:47 -0600

    Task-5: Se crea el proyecto para poder realizar las pruebas.

---
+++ XKCD-Comic.Tests/PruebasUnitarias.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace XKCD_Comic.Tests
+{
+    public class PruebasUnitarias
+    {
+        [Fact]
+        public void Get_Todays_Comic()
+        {
+
+        }
+    }
+}
